0b87bb7b031c72084596385f63e367aa21bdc63f
Helio de Jesus dos Santos Junior <84392613+HelioJr548@users.noreply.github.com>
2024-03-01 21:36:55 -0300

    Módulos CommonJs

---
+++ JS-Moderno/modulos/commonjs/index.js
@@ -0,0 +1,10 @@
+// Para rodar no console: node JS-Moderno/modulos/commonjs/index.js
+
+const render = require("./render");
+const store = require("./store");
+
+console.log("Aplicação iniciada.");
+render();
+store();
+console.log("Aplicação finalizada.");
+
