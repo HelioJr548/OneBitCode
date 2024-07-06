6fc5ef5e9dbe497ad89f8a5feb444ace9609b94a
HelioJr548 <heliojr548@gmail.com>
2024-07-06 10:26:40 -0300

    Assincronicidade

---
+++ JS-Topicos-Avancados/assincronicidade/index.js
@@ -0,0 +1,15 @@
+// Codigo Sincrono:
+function step02() {
+    console.log('Passo 02')
+}
+
+console.log('Passo 01')
+step02()
+console.log('Passo 03')
+
+// Codigo Assincrono:
+console.log('Passo 04')
+setTimeout(() => {
+    console.log('Passo 05')
+}, 1)
+console.log('Passo 06')
