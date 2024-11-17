3541cc2dda9a5f26d813c1b89113200d8dd171c3
HelioJr548 <heliojr548@gmail.com>
2024-11-17 16:03:43 -0300

    NodeJS Web Express: Rendering Static HTML Files

--- NodeJS/02-Node-web/03-retornando-arquivos-estaticos/src/server.js
+++ NodeJS/02-Node-web/03-retornando-arquivos-estaticos/src/server.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.static('public')); // sets up the use of static files.
 
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/views/index.html'); // render a static HTML file in response to a request.
+});
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
