8afc6a060ce5b5e907c5cf4d6b240aebbffd96fb
HelioJr548 <heliojr548@gmail.com>
2024-11-17 12:46:29 -0300

    NodeJS Web: Add simple Routes

--- NodeJS/02-Node-web/http/server.js
+++ NodeJS/02-Node-web/http/server.js
@@ -1,12 +1,26 @@
 const http = require('node:http');
 
 const server = http.createServer((request, response) => {
-	console.log(request);
-	response.writeHead(200);
-	response.write('Servidor HTTP em Node.js funcionando!');
-	response.end();
-});
-
+	const path = request.url
+  
+	switch (path) {
+	  case '/':
+		response.writeHead(200)
+		response.write('Você está na página inicial!')
+		break;
+	  case '/artigos':
+		response.writeHead(200)
+		response.write('Você está na página "artigos"!')
+		break;
+	  default:
+		response.writeHead(404)
+		response.write('Caminho não encontrado!')
+		break;
+	}
+  
+	response.end()
+  })
+  
 const PORT = 3000
 
 server.listen(PORT, () => {
