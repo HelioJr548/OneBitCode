bb31a7611c4c2d5b2fbc671378553562ff1bc3a4
HelioJr548 <heliojr548@gmail.com>
2024-11-20 14:20:10 -0300

    Hands-on Project: All features implemented

--- NodeJS/02-Node-web/projeto-pratico/01-blog-mvc/src/routes.js
+++ NodeJS/02-Node-web/projeto-pratico/01-blog-mvc/src/routes.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const postsController = require('./controllers/postController');
+const adminController = require('./controllers/adminController');
 const router = express.Router();
 
+// Blog routes
 router.get('/', postsController.index);
 router.get('/posts/:id', postsController.show);
+
+// Admin routes
+router.get('/admin', adminController.index);
+router.get('/admin/create', adminController.create);
+router.get('/admin/edit/:id', adminController.edit);
+router.post('/admin/create', adminController.save);
+router.post('/admin/update/:id', adminController.update);
+router.post('/admin/delete/:id', adminController.delete);
 
 module.exports = router;
