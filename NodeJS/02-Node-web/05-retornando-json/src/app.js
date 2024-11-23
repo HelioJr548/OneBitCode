77d34a5a342f0a612ca01bf4b0305c94d8e1734d
HelioJr548 <heliojr548@gmail.com>
2024-11-22 21:50:31 -0300

    API REST: POST routes

--- NodeJS/02-Node-web/05-retornando-json/src/app.js
+++ NodeJS/02-Node-web/05-retornando-json/src/app.js
@@ -3,8 +3,12 @@ const gamesController = require('./controllers/gamesController');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json()); // Permite a leitura do corpo das requisições no formato JSON
+
 app.get('/games', gamesController.index);
 app.get('/games/:id', gamesController.show);
+app.post('/games', gamesController.save);
+app.post('/games/:id/genres', gamesController.addGenre);
 
 app.listen(port, () =>
 	console.log('> Server is up and running on port : ' + port)
